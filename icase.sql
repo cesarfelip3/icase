8357cab2c8943b1b326f97b0e321a8fecc8bcc31
Cesar Felipe <cesarfelip3@gmail.com>
2013-07-15 20:39:24 -0600

    view for orders and db (not good)

--- icase.sql
+++ icase.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Jul 16, 2013 at 02:05 AM
+-- Generation Time: Jul 16, 2013 at 03:35 AM
 -- Server version: 5.5.29-log
 -- PHP Version: 5.3.21
 
@@ -23,6 +23,74 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `billing`
+--
+
+CREATE TABLE IF NOT EXISTS `billing` (
+  `user_id` int(10) unsigned NOT NULL,
+  `billing_address` varchar(50) DEFAULT NULL,
+  `billing_address2` varchar(50) DEFAULT NULL,
+  `billing_state` varchar(50) DEFAULT NULL,
+  `billing_zip` varchar(50) DEFAULT NULL,
+  `billing_country` varchar(50) DEFAULT NULL,
+  `created` datetime DEFAULT NULL,
+  `modified` datetime DEFAULT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+CREATE TABLE IF NOT EXISTS `orders` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) DEFAULT NULL,
+  `body` text,
+  `created` datetime DEFAULT NULL,
+  `modified` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `posts`
+--
+
+CREATE TABLE IF NOT EXISTS `posts` (
+  `case_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) DEFAULT NULL,
+  `case_image` varchar(50) DEFAULT NULL,
+  `created` datetime DEFAULT NULL,
+  `modified` datetime DEFAULT NULL,
+  PRIMARY KEY (`case_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `shipping`
+--
+
+CREATE TABLE IF NOT EXISTS `shipping` (
+  `user_id` int(10) unsigned NOT NULL,
+  `shipping_address` varchar(50) DEFAULT NULL,
+  `shipping_address2` varchar(50) DEFAULT NULL,
+  `shipping_city` varchar(50) DEFAULT NULL,
+  `shipping_state` varchar(50) DEFAULT NULL,
+  `shipping_zip` varchar(50) DEFAULT NULL,
+  `hipping_country` varchar(50) DEFAULT NULL,
+  `body` text,
+  `created` datetime DEFAULT NULL,
+  `modified` datetime DEFAULT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
@@ -34,7 +102,15 @@ CREATE TABLE IF NOT EXISTS `users` (
   `created` datetime DEFAULT NULL,
   `modified` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `password`, `created`, `modified`) VALUES
+(1, NULL, 'cesarfelip3@gmail.com', '1234', '2013-07-16 02:27:10', '2013-07-16 02:27:10'),
+(2, NULL, 'cesarfelip3@gmail.com', '123456', '2013-07-16 02:28:29', '2013-07-16 02:28:29');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
