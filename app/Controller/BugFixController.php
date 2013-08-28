f7862c8ae559efa7dc9f29dd688904687b7f427c
hellomaya <hello@hellomatoMacBook-Pro.local>
2013-08-28 15:39:28 +0800

    fix

--- app/Controller/BugFixController.php
+++ app/Controller/BugFixController.php
@@ -19,6 +19,7 @@ class BugFixController extends AppController {
         $data = $this->Category->find('all');
 
         foreach ($data as $key => $value) {
+            print_r ($value['Category']['slug']);
             if (strpos("/", $value['Category']['slug']) !== false) {
                 print_r ($value['Category']['slug']);
                 $value['Category']['slug'] = preg_replace("/\/+/i", "-", $value['Category']['slug']);
